31fcd7c8984e910c11c7433803fcdf5e210d34c4
Mohamed Ayman <mohayman2007@gmail.com>
2023-01-19 02:25:36 +0000

    Project 0x0F - Task 2 - "Add a custom HTTP header with Puppet"

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,89 +1,26 @@
-# Installs and configure Nginx web server
-exec { 'add nginx stable repo':
-  path    => $path,
-  command => 'sudo add-apt-repository ppa:nginx/stable',
+# Installs a Nginx server with custome HTTP header
+
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-# update software packages list
-exec { 'apt cache update':
-  path    => $path,
-  command => 'apt-get update'
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-# Install nginx
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['apt cache update']
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\
+                  \n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-# Add firewall rules
-exec { 'allow HTTP':
-  command => "ufw allow 'Nginx HTTP'",
-  path    => $path,
-  require => Package['nginx']
-}
-
-# Chmod
-exec { 'chmod www folder':
-  path    => $path,
-  command => 'chmod -R 755 /var/www'
-}
-
-# Create index.html
-file { '/var/www/html/index.html':
-  content => "Hello World!\n",
-}
-
-# Create 404
-file { '/var/www/html/404':
-  content => "Ceci n'est pas une page\n",
-}
-
-# add redirection and error page
-file { 'Nginx default config file':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  notify  => Service['nginx'],
-  content =>
-'server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-               root /var/www/html;
-        # Add index.php to the list if you are using PHP
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        add_header \'X-Served-By\' "$hostname";
-        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files $uri $uri/ =404;
-        }
-        error_page 404 /404;
-        location  /404 {
-            internal;
-        }
-        
-        location /redirect_me {
-            return 301 https://www.google.com/;
-        }
-}
-'
-}
-
-# Restart nginx
-exec { 'restart service':
-  path    => $path,
-  command => 'service nginx restart'
-}
-
-# start service nginx
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx']
-}
-
-# Check that config file exists
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  require => Package['nginx']
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
