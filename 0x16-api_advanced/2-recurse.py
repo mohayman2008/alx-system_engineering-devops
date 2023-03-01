25886d0dae6ea26edd4ff0a10ee9186c87ad69d2
Mohamed Ayman <mohayman2007@gmail.com>
2023-03-01 02:55:59 +0000

    Project 0x16 - Task 3 - "Count it!"

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,40 +4,28 @@ the titles of all hot articles for a given subreddit'''
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after="", count=0):
     '''Returns a list containing the titles of all hot articles
     for a given "subreddit", by querying the Reddit API'''
 
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    limit = 100  # Maximum number of posts to be retreived
-    headers = {'user-agent': 'MyApp v.2023'}
-    params = {'raw_json': '1', 'limit': str(limit)}
-    if len(hot_list):
-        params['after'] = hot_list[-1]['name']
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"User-Agent": 'MyApp v.2023'}
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code == 404:
         return None
-    try:
-        results = response.json()
-        data = results.get('data')
-        if data is None:
-            return None
-        posts = data.get('children')
-        if posts is None:
-            return None
 
-        for post in posts:
-            post_data = post.get('data')
-            if post_data:
-                hot_list.append(post_data)
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
-        if not len(posts) or len(posts) % limit != 0:
-            if not len(hot_list):
-                return None
-            else:
-                return [post.get('title') for post in hot_list]
-        return recurse(subreddit, hot_list)
-
-    except ValueError:
-        return None
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
