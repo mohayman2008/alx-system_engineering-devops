87e75b7f84eaf9500cae02fa9128dd64059d4a01
Mohamed Ayman <mohayman2007@gmail.com>
2023-03-10 01:54:10 +0000

    Project 0x1B - Task 1 - "User limit"

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # Removing "number of opened files" limit for user "holberton" in Project 0x1B Task 1
 
 exec {'remove limits':
-    path   => $path,
+    path    => $path,
     command => 'sed -i "/^.*holberton.*nofile.*$/d" "/etc/security/limits.conf"',
   }
