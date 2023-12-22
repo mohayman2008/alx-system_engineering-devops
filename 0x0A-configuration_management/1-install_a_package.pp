2aff1a526d1793c3314b3e557e3d07f6c6171abd
Mohamed Ayman <mohayman2007@gmail.com>
2023-12-22 16:04:59 +0000

    0x0A. Configuration management - All tasks - 0 --> 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # This manifest executes a "pkill" command to kill a process named "killmenow"
 
+package { 'Werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3',
+  before   => Package['flask']
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
